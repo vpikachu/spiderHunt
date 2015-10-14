59221a6db95502acb10ddb7bce1f21b3c0898ed3
vpikachu <vpikachu@yandex.ru>
2015-10-14 23:12:04 +0200

    1)implement resizeble style of app 2)best way to run game manu

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -5,65 +5,80 @@ var GameBoard = new PIXI.Container();
 GameBoard.init1 = function(){
 
     var bg = new PIXI.Sprite(Game.assets.gameboard.texture);
-    var sf = Game.screen.width/Game.assets.gameboard.texture.width;
-    bg.scale.set(sf,sf);
     bg.anchor.set(0,1);
-    bg.position.set(0,Game.screen.height);
+    bg.position.set(0,Game.designResolution.height);
 
     GameBoard.addChild(bg);
 
 
     var back  = new PIXI.Sprite( Game.assets.backbutton.texture);
     back.anchor.set(0,1);
-    back.scale.set(sf,sf);
-    back.position.set(5,Game.screen.height-7);
+    back.position.set(9,Game.designResolution.height-10);
     back.buttonMode = true;
     back.interactive = true;
     back.tap = GameBoard.showMenu;
     back.click = GameBoard.showMenu;
     GameBoard.addChild(back);
 
+
+    var spider = new PIXI.Sprite(Game.assets.spyder1.texture);
+    spider.position.set(60,Game.designResolution.height-10);
+    spider.anchor.set(0,1);
+    GameBoard.addChild(spider);
+
     GameBoard.state = new PIXI.Text("text",{
-            font: '17px HennyPenny',
+            font: '20px HennyPenny',
             fill: '#FFFFFF'
             /*wrap:true,
-            wrapWidth:600,
-            lineHeight:25*/
+             wrapWidth:600,
+             lineHeight:25*/
         }
     );
     GameBoard.state.anchor.set(0,1);
-    var spider = new PIXI.Sprite(Game.assets.spyder1.texture);
-
-    spider.position.set(15+back.position.x+back.width,Game.screen.height-10);
-    spider.anchor.set(0,1);
-    //spider.scale.set(sf,sf);
-
-    GameBoard.addChild(spider);
     GameBoard.state.position.set(spider.position.x+spider.width+3,spider.position.y);
-    GameBoard.state.scale.set(sf,sf);
     GameBoard.addChild(GameBoard.state);
     PlayGround.addChild(GameBoard);
-};
 
+
+    /***create game process  menu************/
+
+    this.menu = new PIXI.Container();
+    var resume = new MsgBox();
+    resume.text.text = "Resume";
+    resume.y -= resume.children[0].height/2;
+    resume.children[0].click = GameBoard.hideMenu
+    resume.children[0].tap = GameBoard.hideMenu;
+    resume.children[1].style.font = "35px HennyPenny";
+
+    this.menu.addChild(resume);
+
+
+    var  mainmenu = new MsgBox();
+    mainmenu.text.text = "Main Menu";
+    mainmenu.y += mainmenu.children[0].height/2;
+    mainmenu.children[0].scale.x = -1;
+    mainmenu.children[0].click = GameBoard.goMainMenu;
+    mainmenu.children[0].tap = GameBoard.goMainMenu;
+    mainmenu.children[1].style.font = "35px HennyPenny";
+    this.menu.addChild(mainmenu);
+
+};
+GameBoard.hideMenu = function (){
+    PlayGround.isMessageShowing = false;
+    Game.stage.removeChild(GameBoard.menu);
+};
+GameBoard.goMainMenu = function () {
+    GameBoard.hideMenu();
+    Game.showStartMenu();
+};
+GameBoard.showMenu = function () {
+    PlayGround.isMessageShowing = true;
+    Game.stage.addChild(GameBoard.menu);
+};
 GameBoard.updateState = function(){
   GameBoard.state.text = " x "+Game.lives;
     if(Math.isNumeric(Game.level)) GameBoard.state.text += "   level: "+Game.level;
     else
         GameBoard.state.text += "   level: "+Game.level.name;
     GameBoard.state.text += "   score: "+ Game.score;
-};
-GameBoard.showMenu = function () {
-    if(PlayGround.isMessageShowing) return;
-
-    var msgResume = new MsgBox();
-    msgResume.position.y -= msgResume.height/2;
-    var msgMainMenu = new MsgBox();
-    msgMainMenu.position.y += msgMainMenu.height/2;
-    msgMainMenu.getChildAt(0).scale.x = -msgMainMenu.getChildAt(0).scale.x
-    msgMainMenu.separateBoxes.push(msgResume);
-    msgResume.separateBoxes.push(msgMainMenu);
-
-    msgMainMenu.showMsg("Menu",function(){Game.showStartMenu();});
-    msgResume.showMsg("Resume",function(){});
-
 };
